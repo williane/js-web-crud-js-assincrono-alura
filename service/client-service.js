e2d5cebceb28755c31fe22cc9cfc588d10117139
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2021-02-15 13:58:45 -0300

    primeira aula

---
+++ service/client-service.js
@@ -0,0 +1,44 @@
+const criarNovaLinha = (nome, email, id) => {
+  const linhaNovoCliente = document.createElement("tr");
+  const conteudo = ` <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
+            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        </ul>
+    </td>`;
+
+  linhaNovoCliente.innerHTML = conteudo;
+  return linhaNovoCliente;
+};
+
+const tabela = document.querySelector("[data-tabela]");
+
+const listaClientes = () => {
+  const promise = new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
+
+    http.open("GET", "http://localhost:3000/profile");
+    http.send();
+
+    http.onload = () => {
+      if (http.status >= 400) {
+        reject(JSON.parse(http.response));
+      } else {
+        resolve(JSON.parse(http.response));
+      }
+    };
+  });
+
+  return promise;
+};
+
+listaClientes()
+.then((data) => {
+  data.forEach((elemento) => {
+    tabela.appendChild(
+      criarNovaLinha(elemento.nome, elemento.email, elemento.id)
+    );
+  });
+});
